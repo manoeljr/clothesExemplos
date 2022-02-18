3986a8e80edf40809ece8c000b3029597a693ce8
Manoeljr <manoelv8@gmail.com>
2022-02-18 15:36:03 -0300

    Adicionando um validador para o email e full_name

--- schemes/base_user.py
+++ schemes/base_user.py
@@ -1,6 +1,22 @@
-from pydantic import BaseModel
+from email_validator import EmailNotValidError
+from pydantic import BaseModel, validator, validate_email
 
 
 class BaseUser(BaseModel):
     email: str
     full_name: str
+
+    @validator("email")
+    def valida_email(cls, value):
+        try:
+            validate_email(value)
+            return value
+        except EmailNotValidError:
+            raise ValueError("Email is not valid")
+
+    @validator("full_name")
+    def valida_full_name(cls, value):
+        try:
+            first_name, last_name = value.split()
+        except Exception:
+            raise ValueError("You should provide at least 2 names")
